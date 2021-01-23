960df055a3df7714e016da5d91fcfd58946f69c5
Sara <saragorleta@yahoo.com>
2021-01-23 14:24:24 +0100

    aggiunto icons html

--- js/script.js
+++ js/script.js
@@ -104,11 +104,59 @@ const icons = [
       family: 'fas',
     }
   ];
-// ragionamento Cristina
-// -creare due array uno per colori
-// -uno per tipologie
-// const colore=[blue, orange, violet];
-// const type=[animal, vegetables, user];
+  // -creare due array uno per colore e l'altro per type
+  //creiamo un array con i tre colori che poi si dovranno associare ad:
+   // animal vegetables e usur
+const colore=[blue, orange, violet];
+// // il nostro obiettivo è avere questo array per poi associare i colori:
+// // const tipologie =[animal, vegetables, user];
+//         // quindi:
+//   // cicliamo sull'array di partenza
+//   const tipologie=[];
+//   icons.forEach((element) => {
+//     if(!tipologie.includes(element.type)){ //se non lo include(type)
+//     tipologie.push(element.type); //allora aggiungiamo l'elemento typle all'interno dell'array
+//     }
+//   });
+  // console.log(tipologie);
+//cosi otteniamo: array['animal''vegetable''user']   <--
+
+// possiamo trasformare in una funzione (riga 116 a 121)
+function getTypes(array){
+  const tipologie=[];
+  icons.forEach((element) => {
+    if(!tipologie.includes(element.type)){
+    tipologie.push(element.type);
+    }
+  });
+  return tipologie; //restituisce l'array filitrato
+}
+//dopo aver creato tutta la funzione getTypes
+//la richiamiamo creando una nuova const "iconTypes" che,
+// equivale al nuovo array di icons
+const newIcons=getTypes(icons);
+console.log(newIcons);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -120,32 +168,32 @@ const icons = [
 // Prendete i colori da un altro array.***
 
 //preparo i colori
-const blue = '#1e5bb3';
-const orange = '#d99419';
-const violet = '#4b2675';
-// li inserisco nell'array
-const colore=[blue, orange, violet];
-
-//con map associo i colori ad ogni categoria
-const newIcons = icons.map((element) => {
-  return {
-    ...element,
-    colore: {
-      color: (element.type == 'animal') ? blue,
-      color:(element.type== 'vegetable') ? orange : violet
-    }
-  }
-});
-
-console.log(newIcons);
-
-//***Stampare poi tutte le icone utilizzando il template literal.***
-
-icon.forEach((element) => {
-  const {name, prefix,type,family,colore} = element;
-  document.getElementById('nuove-icone').innerHTML += `
-  <div>
-    Nome: ${name} - Prefix: ${prefix} - tipo: ${type} - famiglia: ${family} - colore: ${color}
-  </div>
-  `;
-});
+// const blue = '#1e5bb3';
+// const orange = '#d99419';
+// const violet = '#4b2675';
+// // li inserisco nell'array
+// const colore=[blue, orange, violet];
+//
+// //con map associo i colori ad ogni categoria
+// const newIcons = icons.map((element) => {
+//   return {
+//     ...element,
+//     colore: {
+//       color: (element.type == 'animal') ? blue,
+//       color:(element.type== 'vegetable') ? orange : violet
+//     }
+//   }
+// });
+//
+// console.log(newIcons);
+//
+// //***Stampare poi tutte le icone utilizzando il template literal.***
+//
+// icon.forEach((element) => {
+//   const {name, prefix,type,family,colore} = element;
+//   document.getElementById('nuove-icone').innerHTML += `
+//   <div>
+//     Nome: ${name} - Prefix: ${prefix} - tipo: ${type} - famiglia: ${family} - colore: ${color}
+//   </div>
+//   `;
+// });
