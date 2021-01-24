564b899d6826e95d5981cc38aa22ff177618ecca
Sara <saragorleta@yahoo.com>
2021-01-24 15:26:19 +0100

    eseguito js e commentato i passaggi

--- js/script.js
+++ js/script.js
@@ -104,24 +104,24 @@ const icons = [
       family: 'fas',
     }
   ];
-  // -creare due array uno per colore e l'altro per type
+
+  // -OBIETTIVO:CREARE DUE ARRAY UNO PER COLORE E L'ALTRO PER TYPE
   //creiamo un array con i tre colori che poi si dovranno associare ad:
-   // animal vegetables e usur
+   // animal vegetables e user
 const colore=[blue, orange, violet];
-// // il nostro obiettivo è avere questo array per poi associare i colori:
-// // const tipologie =[animal, vegetables, user];
-//         // quindi:
-//   // cicliamo sull'array di partenza
+
+// Per arrivare ad ottenere questo array [animal, vegetables, user]
+// cicliamo sull'array di partenza (icons):
 //   const tipologie=[];
 //   icons.forEach((element) => {
 //     if(!tipologie.includes(element.type)){ //se non lo include(type)
-//     tipologie.push(element.type); //allora aggiungiamo l'elemento typle all'interno dell'array
+//     tipologie.push(element.type); //allora aggiungiamo l'elemento typle all'interno dell'array vuoto 'tipologie'
 //     }
 //   });
   // console.log(tipologie);
 //cosi otteniamo: array['animal''vegetable''user']   <--
 
-// possiamo trasformare in una funzione (riga 116 a 121)
+// possiamo trasformare in una funzione (da riga 115 a 120)
 function getTypes(array){
   const tipologie=[];
   icons.forEach((element) => {
@@ -129,13 +129,76 @@ function getTypes(array){
     tipologie.push(element.type);
     }
   });
-  return tipologie; //restituisce l'array filitrato
+  return tipologie; //restituisce l'array filtrato ['animal''vegetable''user']
 }
-//dopo aver creato tutta la funzione getTypes
-//la richiamiamo creando una nuova const "iconTypes" che,
-// equivale al nuovo array di icons
-const newIcons=getTypes(icons);
-console.log(newIcons);
+//RICHIAMO lLA FUNZIONE GetTypes
+// creando una nuova const "newArrayTypes" che,
+// equivale al nuovo array['animal''vegetable''user']
+// (per comodità lo mettiamo in una const).
+const newArrayTypes=getTypes(icons);
+console.log(newArrayTypes);
+
+// mappiamo l'array newArrayTypes per conoscere la sua
+// posizione, il suo indice per poi aggiungere il colore
+// e creiamo una nuova const iconsColor
+const iconsColor=icons.map(element) => {
+  // con l'indexOf lui mi va a vedere quale indice(posizione) ha l'elemento type
+  //sul newArrayTypes
+  indexTypes=newArrayTypes.indexOf(element.type);
+  console.log(indexTypes);
+});
+return {
+  ...element,  //mi devi restituire tutti gli elementi(CON INDICE CALCOLATO)
+  color: colors[indexTypes]; //e associargli il colore in base all'indice
+}
+console.log(iconsColor);// otteniamo i colori associati al tipo
+
+// STAMPIAMO LE ICONE CON I COLORI:
+// 1) creiamo la funzione:
+function printIcons(element, container){
+  array.forEach((element) => {
+    const {name, family, prefix, type, color}=element;//abbiamo destrutturato
+
+  container.append(`
+    <div class="icon">
+      <i class="${family} ${prefix} ${name} ${type} ${color}"></i>
+      <div class="title">"${name.toUpperCase()}"</div>
+    </div>`
+  )
+  });
+}
+// 2)richiamiamo la funzione:
+const contenitoreIcone = $('.icons'){ //.icons è il nome classe in html
+  printIcons (iconsColor, container);
+});
+
+// STAMPIAMO IL FILTRO:
+// 1)creiamo la funzione:
+function printFilter(array, select){
+  array.forEach((element) => { //con array consideriamo quello
+                              //che abbiamo creato prima newArrayTypes
+
+    select.append(`
+      <option value="${element}"">${element}</option>
+    `)
+    //<option value="${element}">All</option> element è riferito agli elementi
+    //contenuti nell'array newArrayTypes(animal vegetables user)
+  });
+}
+// 2)richiamo la funzione:
+const seleziona = $('#types'){
+  printFilter(newArrayTypes,select);
+});
+    //ma avremmo potuto scrivere anche cosi:
+//cicliamo su array newArrayTypes
+//newArrayTypes.forEach((element) => {
+//selezioniamo #types e aggiungiamo
+//   $('#types').append(`
+  //element(=all), animal , vegetables user
+//     <option value="${element}"">${animal}${vegetables}${user}</option>
+//     `
+//   )
+// });
 
 
 
